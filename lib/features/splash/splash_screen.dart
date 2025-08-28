a984b489783ad8ba581327a5ff71d6ebd6831e42
Ahmed Maher <maher.ahmed1200a@gmail.com>
2025-08-28 03:19:46 +0300

    Add Nectar UI project files

---
+++ lib/features/splash/splash_screen.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:nectar_ui/core/constants/app_images.dart';
+import 'package:nectar_ui/core/functions/navigation.dart';
+import 'package:nectar_ui/core/utils/colors.dart';
+import 'package:nectar_ui/features/auth/pages/location_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState(); 
+    Future.delayed(Duration(seconds: 3), () {
+     pushWithReplacement(context,const LocationScreen());
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.primaryColor,
+      body: Center(
+        child: SvgPicture.asset(AppImages.logoSvg),
+      ),
+    );
+  }
+}
